b6834f0fdee572e57089556af2c38629b1ecec99
DistilledCode <107433905+DistilledCode@users.noreply.github.com>
2022-06-25 18:18:48 +0530

    upper bound on ids saved in db

--- arck.py
+++ arck.py
@@ -1,8 +1,8 @@
 import re
 import sqlite3
 import time
-from collections import namedtuple
 from contextlib import contextmanager
+from math import ceil
 from typing import Generator
 
 import praw
@@ -12,6 +12,7 @@ from praw.models.reddit.comment import Comment
 from praw.models.reddit.submission import Submission
 
 DB_SOURCE = "seen.db"
+DB_MAX_ROWS = 21
 
 pattern = r"comments\/([a-z0-9]{1,})\/"
 nlp = spacy.load("en_core_web_lg")
@@ -33,12 +34,31 @@ def update_fetched_ids(fetched_ids: set, postid: str):
     with load_db(DB_SOURCE) as con:
         cur = con.cursor()
         cur.execute(
-            """INSERT INTO seen
-                    VALUES (?,?);""",
+            """INSERT OR REPLACE INTO seen
+                VALUES (?,?);""",
             (postid, curr_time),
         )
     fetched_ids.add((postid, curr_time))
     return fetched_ids
+
+
+def bisect_db() -> int:
+    with load_db(DB_SOURCE) as con:
+        cur = con.cursor()
+        cur.execute(
+            """DELETE FROM seen 
+                WHERE postid IN (
+                    SELECT postid
+                    FROM seen 
+                    ORDER BY time_seen 
+                    LIMIT (
+                        SELECT COUNT(*) 
+                        FROM seen
+                    )/2
+                );
+            """
+        )
+    return get_fetched_ids()
 
 
 def get_fetched_ids() -> set:
@@ -47,7 +67,7 @@ def get_fetched_ids() -> set:
         cur.execute(
             """CREATE TABLE IF NOT EXISTS 
                     seen(
-                        postid TEXT NOT NULL,
+                        postid TEXT NOT NULL UNIQUE,
                         time_seen REAL NOT NULL
                         );
                     """
@@ -86,22 +106,22 @@ def prp_ratio(comment: Comment) -> float:
     prp_count = sum(True for token in doc if token.tag_ in personal_pronouns)
     ratio = prp_count / len(doc)
     if ratio > 0.1:
-        print(f"\n\n[{ratio}] {comment.body}\n\n")
+        print(f"\n[{ratio}] {comment.body}\n")
     return ratio
 
 
 def validate_comment(comment: Comment) -> bool:
     if comment.score < 50:
-        print("Low Karma!\n\n")
+        print("Low Karma!\n")
         return False
     if comment.edited is not False:
-        print("edited!\n\n")
+        print("edited!\n")
         return False
     if comment.stickied is True:
-        print("Stickied!\n\n")
+        print("Stickied!\n")
         return False
     if comment.author is None:
-        print("deleted or removed\n\n")
+        print("deleted or removed\n")
         return False
     if prp_ratio(comment) > 0.1:
         return False
@@ -116,38 +136,40 @@ def update_preferences(googled: Submission) -> None:
 
 
 def print(text) -> None:
-    with open("log.txt", "a") as f:
+    with open("log.txt", "a", encoding="utf-8") as f:
         f.write(text)
 
 
-def validate_post(post, fetched_ids) -> tuple[bool, bool]:
-    Validation = namedtuple("Validation", ["is_valid", "is_unique"])
-    validation = Validation(is_unique=True, is_valid=True)
+def validate_post(post, fetched_ids) -> dict[bool]:
+    validation = {"is_unique": True, "is_valid": True}
 
     if (post.id,) in fetched_ids:
-        validation._replace(is_unique=False)
+        validation["is_unique"] = False
 
     # average token lenght of top 1000 posts is < 14
     if len(nlp(post.title)) > 20:
-        print(f"{'[SKipping long question]':-^40}\n\n")
-        validation._replace(is_valid=False)
+        validation["is_valid"] = False
     return validation
 
 
-print("STARTING NEW SESSION\n\n")
+print("\n@@@\nSTARTING NEW SESSION\n@@@")
 
-fetched_ids = get_fetched_ids()
-
-for asked in subreddit.stream.submissions(skip_existing=True):
-    post = validate_post(asked, fetched_ids)
-    if post.is_unique:
+fetched_ids: set = get_fetched_ids()
+for asked in subreddit.new(limit=DB_MAX_ROWS):
+    print("\n~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~\n")
+    print(f"\nasked: {asked.title}")
+    post: dict = validate_post(asked, fetched_ids)
+    if post["is_unique"]:
+        print(" \nUnique Post")
         fetched_ids = update_fetched_ids(fetched_ids, asked.id)
-    if not post.is_unique or not post.is_valid:
+    if len(fetched_ids) == DB_MAX_ROWS:
+        fetched_ids = bisect_db()
+        print(f"\n{len(fetched_ids)=}")
+        assert len(fetched_ids) == ceil(DB_MAX_ROWS / 2)
+    if not post["is_unique"] or not post["is_valid"]:
+        print(" \nInvalid Post")
         continue
-    print("~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~====~=\n")
-    print("~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~====~=\n")
-    print("~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~====~=\n\n")
-    print(f"asked:   {asked.title}\n\n")
+
     query = f"site:www.reddit.com/r/askreddit {asked.title}"
     candidates = []
     for searched in search(query=query, num=3, stop=3, country="US"):
@@ -156,15 +178,15 @@ for asked in subreddit.stream.submissions(skip_existing=True):
         update_preferences(googled)
         if post_age(googled) < 14:
             continue
-        print(f"googled: {googled.title}\n\n")
+        print(f"googled: {googled.title}\n")
         similarity = calculate_similarity(asked.title, googled.title)
-        print(f"score={googled.score}; similar={round(similarity,4)}\n\n")
+        print(f"score={googled.score}; similar={round(similarity,4)}\n")
         if similarity > 0.95 and googled.score > 100:
-            print("GOT ONE!\n\n")
+            print("GOT ONE!\n")
             candidates.extend(comment for comment in googled.comments)
         else:
-            print("Googled post didnt met criteria\n\n")
-        print("************************************************\n\n")
+            print("Googled post didnt met criteria\n")
+        print("************************************************\n")
 
     if candidates:
         valid_comments = []
@@ -174,12 +196,14 @@ for asked in subreddit.stream.submissions(skip_existing=True):
 
         valid_comments.sort(key=lambda x: x.score, reverse=True)
         for top_comment in valid_comments:
-            print("@@@@@@@@@@@@@@@@\n\n")
-            print(f"[{top_comment.score}] {top_comment.body}\n\n")
-            print("@@@@@@@@@@@@@@@@\n\n")
+            print("@@@@@@@@@@@@@@@@\n")
+            print(f"[{top_comment.score}] {top_comment.body}\n")
+            print("@@@@@@@@@@@@@@@@\n")
     else:
-        print("No googled post had >100 score or >0.95 similarity\n\n")
+        print("No googled post had >100 score or >0.95 similarity\n")
 
 
 # TODO periodically look at 'rising' posts also as bot will sleep after commenting, missing out on a lot
-# TODO implementing the above will also need to keep track of which posts have been visited
+# TODO warning when `DB_MAX_ROWS` is set too low (minimum 100 is advised)
+
+# * replace all cur = con.cursor() with only con?
